992a8c3bb0ef79b454ec8ebc370a59186bc26057
bee-keeper <ricbottomley@gmail.com>
2014-11-19 17:40:44 +0000

    naming

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(
   name = 'pygnip-allapis',
-  packages = ['pygnip-allapis'],
+  packages = ['pygnip_allapis'],
   version = '0.1',
   description = 'A wrapper around the GNIP APIs',
   author = 'https://github.com/bee-keeper',
-  author_email = '',
+  author_email = 'none@none.com',
   url = 'https://github.com/bee-keeper/pygnip-allapis.git',
   download_url = 'https://github.com/bee-keeper/pygnip-allapis/tarball/0.1',
   keywords = ['gnip', 'api', 'wrapper'],
